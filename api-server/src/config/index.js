227bd8e41cafc9ecdbc1169e033ec44d689d344b
Ani_03 <140332086+Anidipta@users.noreply.github.com>
2025-06-26 21:44:42 +0530

    Update index.js

--- api-server/src/config/index.js
+++ api-server/src/config/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   port: process.env.PORT || 3000,
-  mongodbUri: process.env.MONGODB_URI || 'mongodb+srv://anidiptapal:ani123@cluster0.yzbnt.mongodb.net/',
+  mongodbUri: process.env.MONGODB_URI || '',
   natsUrl: process.env.NATS_URL || 'nats://localhost:4222',
   coinGeckoApiUrl: process.env.COINGECKO_API_URL || 'https://api.coingecko.com/api/v3',
   supportedCoins: ['bitcoin', 'ethereum', 'matic-network']
